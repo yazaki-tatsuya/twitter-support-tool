75082841fc4b67ae81dce604cf19fbd9c8da8642
yazaki-tatsuya <yazaki.tatsuya.0813@gmail.com>
2021-03-07 23:59:59 +0900

    feat(hashtag-search): enhance hashtag search with list models and CSV export

--- TwitterSupportTool/src/twitter/TwitterHashTagSearchWithFav.java
+++ TwitterSupportTool/src/twitter/TwitterHashTagSearchWithFav.java
@@ -13,11 +13,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.HashTagSearchList;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
+import utils.DbConnectUtil3;
 import utils.RoutingTable;
 
 @WebServlet(RoutingTable.hashtagV2_sv)
@@ -25,98 +27,124 @@ public class TwitterHashTagSearchWithFav extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		//# キー認証・Twitterクラスのインスタンス生成
-		CommonUtil cu = new CommonUtil();
-		Twitter twitter = cu.getTwitter();		
+		DbConnectUtil3 db = new DbConnectUtil3();
+		if(db.DbUserUseCount(request.getRemoteUser())>=RoutingTable.api_limit) {
+			db.DbClose();
+			//# 遷移先画面
+			String forwardpage = "./LimitExceedError.jsp";
+			
+			//# 画面遷移
+			RequestDispatcher dispatch = request.getRequestDispatcher(forwardpage);
+			dispatch.forward(request, response);					
+		}else {
+			//# DB接続・API利用回数更新
+			db.DbUpdateApiUseCount(request.getRemoteUser());
+			db.DbClose();
 		
-		//#### 変数定義など
-		//# エンコーディング方式の設定
-		request.setCharacterEncoding("UTF-8");
-		response.setHeader("Content-Type", "text/html; charset=UTF-8");
-		//# 検索対象キーワードの取得（nullでない＆""でない場合に取得）
-		String searchTarget = "#"+(String) request.getSession().getAttribute("searchTag");
-		//# 指定いいね数の取得（nullでない＆""でない場合に取得）
-		int favcount = Integer.parseInt((String)request.getSession().getAttribute("searchFav"));
-		System.out.println("#### @Servlet HashTag : "+request.getParameter("searchTag")+" FavCount: "+request.getParameter("searchFav"));
-		
-		request.setAttribute("hashtag", searchTarget);	
-		//# 遷移先画面
-		String forwardpage = RoutingTable.hashtagV2_r;
-		System.out.println("### : Get the forward page = "+forwardpage);
+			//# キー認証・Twitterクラスのインスタンス生成
+			CommonUtil cu = new CommonUtil();
+			Twitter twitter = cu.getTwitterV2(RoutingTable.authkey_max);	
+			
+			//#### 変数定義など
+			//# エンコーディング方式の設定
+			request.setCharacterEncoding("UTF-8");
+			response.setHeader("Content-Type", "text/html; charset=UTF-8");
+			//# 検索対象キーワード & いいね数の取得（nullでない＆""でない場合に取得）
+			String searchTarget = "#"+(String) request.getSession().getAttribute("searchTag");
+			int favcount = Integer.parseInt((String)request.getSession().getAttribute("searchFav"));
+			
+			//# 遷移先画面
+			String forwardpage = RoutingTable.hashtagV2_r;
+			System.out.println("# == [SV] Forward page : "+forwardpage);
 
-		//#### ハッシュタグによる検索
-		//# ハッシュタグの検索用クラス(Query)
-		Query query = new Query();		
-		//# クエリ生成（検索対象のワードをセット＆取得件数セット）
-		query.setCount(200);
-		query.setQuery(searchTarget);
-		query.setSince("2020-11-01");
-		//# 検索結果の格納用
-		QueryResult queryResult = null;
+			//#### ハッシュタグによる検索
+			//# ハッシュタグの検索用クラス(Query)
+			Query query = new Query();		
+			//# クエリ生成（検索対象のワードをセット＆取得件数セット）
+			query.setCount(RoutingTable.hashtag_setcount);
+			query.setSince("2020-11-01");
+			query.setQuery(searchTarget);			
+			//# 検索結果の格納用
+			QueryResult queryResult = null;
 
-		//# 最終結果の格納用
-		List<String> user = new ArrayList<String>();
-		List<Long> tweetid = new ArrayList<Long>();
-		List<String> result = new ArrayList<String>();
-		List<String> date = new ArrayList<String>();
-		List<Integer> favcnt = new ArrayList<Integer>();
-		List<Integer> retcnt = new ArrayList<Integer>();
-		
-		int counter = 0;
-		//# 検索結果から１Statusずつ、ツイート内容を最終結果に格納
-		do {
-			//# クエリの発行・結果格納
-			try {
-				queryResult = twitter.search(query);
-			} catch (TwitterException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			for(Status status : queryResult.getTweets()) {
-				
-				if(!status.isRetweet() && status.getFavoriteCount() > favcount) {
-					//####(5)URLの検出・整形
-					String tmp = status.getText();
-					String regex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-					Pattern pattern = Pattern.compile(regex);
-					Matcher matcher = pattern.matcher(tmp);
-					while(matcher.find()) {
-						String tmp_url = "<a href=\""+matcher.group()+"\" target=\"_blank\">"+matcher.group()+"</a>";
-						tmp = tmp.replace(matcher.group(),tmp_url);
-					}
-					//####(6)日付の加工
-					String pattern2 = "yyyy-MM-dd";
-					SimpleDateFormat sdf = new SimpleDateFormat(pattern2);
-					String date_format = sdf.format(status.getCreatedAt());
-									
-					//####各要素をArrayListに追加
-					user.add(status.getUser().getScreenName());
-					tweetid.add(status.getId());
-					result.add(tmp);
-					date.add(date_format);
-					favcnt.add(status.getFavoriteCount());
-					retcnt.add(status.getRetweetCount());
+			//# 最終結果の格納用
+			HashTagSearchList ht = new HashTagSearchList();
+//			List<String> user = new ArrayList<String>();
+//			List<Long> tweetid = new ArrayList<Long>();
+//			List<String> result = new ArrayList<String>();
+//			List<String> date = new ArrayList<String>();
+//			List<Integer> favcnt = new ArrayList<Integer>();
+//			List<Integer> retcnt = new ArrayList<Integer>();
+			//# ページループカウント
+			int counter = 0;
+			//# 検索結果から１Statusずつ、ツイート内容を最終結果に格納
+			//# hashtagV2_pagelimitで指定したページ分、結果があるまで検索を続ける
+			do {
+				//# クエリの発行・結果格納
+				try {
+					queryResult = twitter.search(query);
+				} catch (TwitterException e) {
+					e.printStackTrace();
 				}
-			}
-			try {
-				Thread.sleep(500);				
-			}catch(InterruptedException e){
-				e.printStackTrace();
-			}
-			counter++;
-			System.out.println("&&&&&&&&&&&&& "+counter);
-		}while((query = queryResult.nextQuery())!=null && counter<20);
-		//#### 結果格納・画面遷移
-		//# 結果をリクエストオブジェクトにセット
-		request.setAttribute("result1",user);
-		request.setAttribute("result2",favcnt);
-		request.setAttribute("result3",result);
-		request.setAttribute("result4",retcnt);
-		request.setAttribute("result5",tweetid);
-		request.setAttribute("result6",date);
-		//# 画面遷移
-		RequestDispatcher dispatch = request.getRequestDispatcher(forwardpage);
-		dispatch.forward(request, response);
-		//doGet(request, response);
+				for(Status status : queryResult.getTweets()) {
+					
+					if(!status.isRetweet() && status.getFavoriteCount() >= favcount) {
+						//####(5)URLの検出・整形
+						String tmp = status.getText();
+						String regex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+						Pattern pattern = Pattern.compile(regex);
+						Matcher matcher = pattern.matcher(tmp);
+						while(matcher.find()) {
+							String tmp_url = "<a href=\""+matcher.group()+"\" target=\"_blank\">"+matcher.group()+"</a>";
+							tmp = tmp.replace(matcher.group(),tmp_url);
+						}
+						//####(6)日付の加工
+						String pattern2 = "yyyy-MM-dd";
+						SimpleDateFormat sdf = new SimpleDateFormat(pattern2);
+						String date_format = sdf.format(status.getCreatedAt());
+										
+						//####各要素をArrayListに追加
+						ht.setTweetId(status.getId());
+						ht.setUser(status.getUser().getScreenName());
+						ht.setContent(tmp);
+						ht.setDate(date_format);
+						ht.setFavCnt(status.getFavoriteCount());
+						ht.setRetCnt(status.getRetweetCount());
+						
+//						user.add(status.getUser().getScreenName());
+//						tweetid.add(status.getId());
+//						result.add(tmp);
+//						date.add(date_format);
+//						favcnt.add(status.getFavoriteCount());
+//						retcnt.add(status.getRetweetCount());
+					}
+				}
+				//# 0.5秒待つ
+				try {
+					Thread.sleep(500);				
+				}catch(InterruptedException e){
+					e.printStackTrace();
+				}
+				counter++;
+				System.out.println("# ==== [SV] searching tweet of page : "+counter);
+			}while((query = queryResult.nextQuery())!=null && counter<RoutingTable.hashtagV2_pagelimit);
+			//#### 結果格納・画面遷移
+			//# 検索キーをセッションに保存（次画面で使用）
+			request.setAttribute("hashtag", searchTarget);
+			request.setAttribute("favcount", favcount);	
+			System.out.println("# == [SV] setAttribute hashtag : "+request.getParameter("searchTag")+" FavCount: "+request.getParameter("searchFav"));
+			
+			//# 結果をリクエストオブジェクトにセット
+			request.setAttribute("HashTagSearchList",ht);
+//			request.setAttribute("result1",user);
+//			request.setAttribute("result2",favcnt);
+//			request.setAttribute("result3",result);
+//			request.setAttribute("result4",retcnt);
+//			request.setAttribute("result5",tweetid);
+//			request.setAttribute("result6",date);
+			//# 画面遷移
+			RequestDispatcher dispatch = request.getRequestDispatcher(forwardpage);
+			dispatch.forward(request, response);		
+		}	
 	}
 }
---
+++ TwitterSupportTool/src/twitter/TwitterHashTagSearchWithFavCsv.java
@@ -0,0 +1,110 @@
+package twitter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import utils.RoutingTable;
+
+@WebServlet(RoutingTable.hashtagV2_csv)
+public class TwitterHashTagSearchWithFavCsv extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//#### キー認証・Twitterクラスのインスタンス生成
+		CommonUtil cu = new CommonUtil();
+		Twitter twitter = cu.getTwitterV2(RoutingTable.authkey_max);
+		
+		//#### 変数定義など
+		//# エンコーディング方式の設定
+		String filename = "HashTagSearchResult.csv";
+		request.setCharacterEncoding("UTF-8");
+		//response.setCharacterEncoding("SHIFT-JIS");
+		//response.setContentType("text/csv; charset=SHIFT-JIS");
+		response.setHeader("Content-Type", "text/csv; charset=SHIFT-JIS");
+		response.setHeader("Content-Disposition", "attachment; filename=\""+filename+"\"");
+		PrintWriter out = response.getWriter();
+		out.append("ユーザーID,").append("Tweet日付").append("TweetのURL,").append("Tweet内容,").append("いいね数,").append("リツイート数\n");
+		
+		//# 検索対象キーワードの取得
+		String searchTarget = "#"+(String) request.getSession().getAttribute("hashtag_val");
+		int favcount = Integer.parseInt(request.getSession().getAttribute("favcount_val").toString());
+		System.out.println("# ======== Generate csv START for : "+searchTarget+" & favcnt="+favcount);
+
+		//#### ハッシュタグによる検索
+		//# ハッシュタグの検索用クラス(Query)
+		Query query = new Query();
+		
+		//# クエリ生成（検索対象のワードをセット＆取得件数セット）
+		query.setCount(RoutingTable.hashtag_setcount);
+		query.setSince("2020-11-01");
+		query.setQuery(searchTarget);
+		//# 検索結果の格納用
+		QueryResult queryResult = null;
+		//# ページループカウント
+		int counter = 0;
+		//# 検索結果から１Statusずつ、ツイート内容を最終結果に格納
+		do {
+			//# クエリの発行・結果格納
+			try {
+				queryResult = twitter.search(query);
+			} catch (TwitterException e1) {
+				e1.printStackTrace();
+			}
+			for(Status status : queryResult.getTweets()) {
+				
+				if(!status.isRetweet() && status.getFavoriteCount() >= favcount) {
+					//#### URLの検出・整形
+					//ツイート内容を一時変数に格納
+					String tmp = status.getText();
+					//URLを抽出する正規表現
+					String regex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+					Pattern pattern = Pattern.compile(regex);
+					Matcher matcher = pattern.matcher(tmp);
+					//URLが検出される間は<a>タグで囲った文言に置き換える
+					while(matcher.find()) {
+						String tmp_url = "<a href=\""+matcher.group()+"\" target=\"_blank\">"+matcher.group()+"</a>";
+						tmp = tmp.replace(matcher.group(),tmp_url);
+					}
+					//その他、置換が必要な文言の処理（csv特有処理）
+					if(tmp.contains("\"")) {tmp = tmp.replace("\"", "\"\"");}
+					if(tmp.contains(",")) {tmp = "\""+tmp+"\"";}
+					if(tmp.contains("\n")) {tmp = tmp.replace("\n","");}
+					if(tmp.contains("\r")) {tmp = tmp.replace("\r","");}
+					//####(6)日付の加工
+					String pattern2 = "yyyy-MM-dd";
+					SimpleDateFormat sdf = new SimpleDateFormat(pattern2);
+					String date_format = sdf.format(status.getCreatedAt());
+					
+					//####各要素をArrayListに追加
+					out.append(status.getUser().getScreenName()+",");
+					out.append(date_format+",");
+					out.append("'"+status.getId()+",");
+					out.append(tmp+",");
+					out.append(status.getFavoriteCount()+",");
+					out.append(status.getRetweetCount()+"\n");
+				}
+			}
+			//# 0.5秒待つ
+			try {
+				Thread.sleep(500);				
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+			counter++;
+		}while((query = queryResult.nextQuery())!=null && counter<RoutingTable.hashtagV2_pagelimit);
+		System.out.println("# ======== Generate csv END for : "+searchTarget);
+	}
+}
