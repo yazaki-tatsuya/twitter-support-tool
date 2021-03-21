b435bb79bdeffe345ad1beacf6e643f14b784851
yazaki-tatsuya <yazaki.tatsuya.0813@gmail.com>
2021-03-21 23:59:59 +0900

    feat(operation-log): add file-based operation logging infrastructure

--- TwitterSupportTool/src/models/OperationLog.java
+++ TwitterSupportTool/src/models/OperationLog.java
@@ -1,32 +1,73 @@
 package models;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class OperationLog {
 	
+	private final String logfile = "/home/admin/eclipse-workspace/TwitterSupportTool/WebContent/custom_logs/operation_logs.txt";
 	private Date op_date = new Date();
 	private String op_user = "";
 	private String op_func = "";
 	private String searchkey = "";
+	private String message = "";
 	
 	//# constructor
-	public OperationLog(Date op_date,String op_user,String op_func,String searchkey){
+	public OperationLog(Date op_date,String op_user,String op_func,String searchkey,String message){
 		this.op_date = op_date;
 		this.op_user = op_user;
 		this.op_func = op_func;
-		this.searchkey = searchkey;		
+		this.searchkey = searchkey;
+		this.message = message;	
 	}
 	
 	//# set
 	public void setOpDate(Date op_date) {this.op_date = op_date;}
-	public void setOpUser(Date op_user) {this.op_date = op_user;}
-	public void setOpFunc(Date op_func) {this.op_date = op_func;}
-	public void setSearchKey(Date searchkey) {this.op_date = searchkey;}
+	public void setOpUser(String op_user) {this.op_user = op_user;}
+	public void setOpFunc(String op_func) {this.op_func = op_func;}
+	public void setSearchKey(String searchkey) {this.searchkey = searchkey;}
+	public void setMessage(String message) {this.message = message;}
 	
 	//# get
 	public Date getOpDate() {return this.op_date;}
 	public String getOpUser() {return this.op_user;}
 	public String getOpFunc() {return this.op_func;}
 	public String getSearchKey() {return this.searchkey;}
+	public String getMessage() {return this.message;}
+	
+	//# write log
+	public void WriteLog() {
+		try{
+			//# ファイル書き出し
+			FileWriter writer = new FileWriter(logfile,true);
+			BufferedWriter bwriter = new BufferedWriter(writer);
+			PrintWriter pwriter = new PrintWriter(bwriter);			
+			//# 日付の整形
+			Date dt = new Date();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+			String formatdate = sdf.format(this.op_date);
+			//# 検索キーが日本語だとprintfがずれる件の対応
+			int zenkaku_count = 0, keylength = 0;
+			char[] c = this.searchkey.toCharArray();
+			for(int i=0; i<c.length; i++) {
+				if(String.valueOf(c[i]).getBytes().length <= 1) {
+					keylength += 1;
+				}else {					
+					zenkaku_count += 1;
+					keylength += 2;
+				}
+			}
+			
+			pwriter.printf("|%-25s|%-10s|%-14s|%-"+(40-zenkaku_count)+"s|%-50s", formatdate,this.op_user,this.op_func,this.searchkey,this.message);
+			pwriter.write("\r\n");
+			pwriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
---
+++ TwitterSupportTool/src/utils/LogMsg.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class LogMsg {
+	//# 定形文
+	public static final String sv1 = "SV_(1)v2";
+	public static final String sv2 = "SV_(2)";
+	public static final String sv3 = "SV_(3)";
+	public static final String sv4 = "SV_(4)v3";
+	public static final String sv5 = "SV_(5)";
+	public static final String sv6 = "SV_(6)v2";
+}
